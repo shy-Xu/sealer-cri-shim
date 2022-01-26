bff20d1093cb1e70b112ba4b2ea6079e682d2477
shy-Xu <1765667234@qq.com>
2022-01-26 18:07:03 +0800

    redirect registry

--- server/service.go
+++ server/service.go
@@ -2,7 +2,19 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"strings"
+
 	api "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+)
+
+const (
+	legacyDefaultDomain = "index.docker.io"
+	defaultDomain       = "docker.io"
+	officialRepoName    = "library"
+	defaultTag          = "latest"
+
+	sealerHub = "sea.hub:5000"
 )
 
 func (s *server) ListImages(ctx context.Context,
@@ -36,6 +48,11 @@ func (s *server) PullImage(ctx context.Context,
 	// note:
 	// but kubelet sometimes will invoke imageService.RemoveImage() or something else. The req.Image.Image will the original name.
 	// so we'd better tag "sealer.hub/library/nginx:1.1.1" with original name "req.Image.Image" After "rsp, err := (*s.imageService).PullImage(ctx, req)".
+	domain, _ := splitDockerDomain(req.Image.Image)
+	if domain == "" {
+		req.Image.Image = sealerHub + "/" + req.Image.Image
+	}
+	fmt.Println(req.Image.Image)
 	rsp, err := (*s.imageService).PullImage(ctx, req)
 
 	if err != nil {
@@ -65,4 +82,20 @@ func (s *server) ImageFsInfo(ctx context.Context,
 	}
 
 	return rsp, err
+}
+
+func splitDockerDomain(name string) (domain, remainder string) {
+	i := strings.IndexRune(name, '/')
+	if i == -1 || (!strings.ContainsAny(name[:i], ".:") && name[:i] != "localhost" && strings.ToLower(name[:i]) == name[:i]) {
+		domain, remainder = "", name
+	} else {
+		domain, remainder = name[:i], name[i+1:]
+	}
+	if domain == legacyDefaultDomain {
+		domain = defaultDomain
+	}
+	if domain == defaultDomain && !strings.ContainsRune(remainder, '/') {
+		remainder = officialRepoName + "/" + remainder
+	}
+	return
 }
